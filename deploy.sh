e5189ac1b58028d6307332f3592b6e67930e71d7
sch-28 <jansch2803@googlemail.com>
2022-08-13 04:30:30 +0200

    added deploy script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd public
+git init
+git branch -m main
+git add -A
+git commit -m 'deploy'
+git remote add origin https://github.com/sch-28/war-analyzer.git
+git push -f -u origin main:github_page
+
+cd -
